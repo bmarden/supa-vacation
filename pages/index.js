90d161cae6a6a9924b41b3bc6a9e40f6ea91ba8e
Ben Marden <benvmarden@gmail.com>
2022-04-27 08:51:32 -0700

    Use real data to populate home page

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,23 @@
 import Layout from '@/components/Layout';
 import Grid from '@/components/Grid';
+import { PrismaClient } from '@prisma/client';
 
-import homes from 'data.json';
+const prisma = new PrismaClient();
 
-export default function Home() {
+export async function getServerSideProps() {
+  const homes = await prisma.home.findMany();
+  return {
+    props: {
+      homes: JSON.parse(JSON.stringify(homes))
+    }
+  };
+}
+
+export default function Home({ homes = [] }) {
   return (
     <Layout>
-      <h1 className="text-xl font-medium text-gray-800">
-        Top-rated places to stay
-      </h1>
-      <p className="text-gray-500">
-        Explore some of the best places in the world
-      </p>
+      <h1 className="text-xl font-medium text-gray-800">Top-rated places to stay</h1>
+      <p className="text-gray-500">Explore some of the best places in the world</p>
       <div className="mt-8">
         <Grid homes={homes} />
       </div>
